60bd9769ee2f160391c4387baa5db9e48eada9f1
André Luiz <silva.ala82@gmail.com>
2024-01-04 21:32:43 -0300

    feat: implements that should be able to upload file and return a converted csv to json

---
+++ app.js
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import multer from "multer";
+import { convertCsvBufferToJsonFile } from "./json-conversor.js";
+
+const app = express();
+const upload = multer({
+  fileFilter: function fileFilter(req, file, cb) {
+    file.mimetype === "text/csv" ? cb(null, true) : cb(null, false);
+  },
+});
+
+app.post("/upload", upload.single("csvFile"), async (req, res) => {
+  if (req.file === undefined)
+    return res.status(200).json({ message: "Tipo de Arquivo inválido" });
+
+  const file = req.file;
+
+  const dataJson = await convertCsvBufferToJsonFile(file.buffer);
+  return res.status(200).json({ message: "ok", data: dataJson });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
--- json-conversor.js
+++ json-conversor.js
@@ -1,8 +1,9 @@
 // JSON format: Objeto
 
 import fs from "node:fs/promises";
+import { Readable } from "node:stream";
 
-async function convertCsvToJson(csvFilePath) {
+export async function convertCsvFileToJson(csvFilePath) {
   const buffer = await fs.readFile(csvFilePath);
 
   const dataCsv = Buffer.from(buffer).toString().split("\n");
@@ -24,4 +25,22 @@ async function convertCsvToJson(csvFilePath) {
   return dataJson;
 }
 
-console.log(await convertCsvToJson(".examples/organization.csv"));
+export async function convertCsvBufferToJsonFile(fileBuffer) {
+  const dataCsv = fileBuffer.toString("utf-8").split("\n");
+
+  const titles = dataCsv[0].split(",");
+  dataCsv.shift();
+
+  const dataJson = dataCsv.map((current) => {
+    const values = current.split(/,(?=(?:[^"]*"[^"]*")*[^"]*$)/g);
+    const newObject = titles.reduce((objAcc, objCurrent, objIndex) => {
+      objAcc[objCurrent] = values[objIndex];
+
+      return objAcc;
+    }, {});
+
+    return newObject;
+  });
+
+  return dataJson;
+}
